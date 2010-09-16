d1b460789dbffaa8793b9c11ebf5e2fc6034f10c
JC <jc@gowalla.com>
2010-09-16 13:29:00 -0500

    tweaking logger

--- app/controllers/logger_controller.rb
+++ app/controllers/logger_controller.rb
@@ -3,10 +3,12 @@ class LoggerController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def log
-    params.delete(:action)
-    params.delete(:controller)
-    params[:user_agent] = params[:user_agent].gsub(/ /, "|") # not sure why, but spaces were breaking popping off the resque queue, so we're replacing them
-    Resque.enqueue(StatsLog, params.to_json)
+    if params[:type]
+      params.delete(:action)
+      params.delete(:controller)
+      params[:user_agent] = params[:user_agent].gsub(/ /, "|") if params[:user_agent] # not sure why, but spaces were breaking popping off the resque queue, so we're replacing them
+      Resque.enqueue(StatsLog, params.to_json)
+    end
     render :json => {}
   end
   
